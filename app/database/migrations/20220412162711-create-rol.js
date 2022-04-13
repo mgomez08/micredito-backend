e19888fee4eaad9c04a08153fb759224fe85424d
Marlon Gómez <marlonjulian15@gmail.com>
2022-04-13 10:04:51 -0500

    Create Rol Migration

---
+++ app/database/migrations/20220412162711-create-rol.js
@@ -0,0 +1,28 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Roles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      role: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Roles");
+  },
+};

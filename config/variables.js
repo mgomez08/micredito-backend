a85eb408ad700e59ae9b82baa493b2a684201ff3
Marlon Gómez <marlonjulian15@gmail.com>
2022-04-14 18:59:41 -0500

    Add variables

--- config/variables.js
+++ config/variables.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
+//VARIABLES API
 const API_VERSION = process.env.API_VERSION;
+
+//VARIABLES DATABASE
 const HOST = process.env.HOST;
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const DATABASE = process.env.DATABASE;
 const PORT_DB = process.env.PORT_DB;
 const DB_DIALECT = process.env.DB_DIALECT;
+
+//VARIABLES JWT AND AUTH
+const SECRET_KEY = process.env.AUTH_SECRET_KEY;
+const EXPIRES_IN = process.env.AUTH_EXPIRES;
+const ROUNDS = parseInt(process.env.AUTH_ROUNDS);
 
 module.exports = {
   API_VERSION,
@@ -16,4 +24,7 @@ module.exports = {
   DATABASE,
   PORT_DB,
   DB_DIALECT,
+  SECRET_KEY,
+  EXPIRES_IN,
+  ROUNDS,
 };

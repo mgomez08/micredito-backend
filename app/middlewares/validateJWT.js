0f2fc6dde8f6d9080d9904781c1e395e6056a63b
Marlon Gómez <marlonjulian15@gmail.com>
2022-04-14 18:59:22 -0500

    Create validateJWT middleware

---
+++ app/middlewares/validateJWT.js
@@ -0,0 +1,32 @@
+const { verifyToken } = require("../utils/jwt");
+const { getUnixTime } = require("date-fns");
+
+const validateJWT = async (req, res, next) => {
+  try {
+    const token = req.header.authorization;
+    if (!token) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No hay token en la cabecera de Autentificación",
+      });
+    }
+    const payload = verifyToken(token);
+    if (payload.exp <= getUnixTime(new Date())) {
+      return res.status(401).json({
+        ok: false,
+        msg: "El token ha expirado",
+      });
+    }
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(404).json({
+      ok: false,
+      msg: "Token no valido",
+    });
+  }
+};
+
+module.exports = {
+  validateJWT,
+};

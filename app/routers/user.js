fcbc9ecfb5c748002a5330a6652033896e8ba822
Marlon Gómez <marlonjulian15@gmail.com>
2022-04-15 17:01:31 -0500

    Add controllers to router

--- app/routers/user.js
+++ app/routers/user.js
@@ -5,5 +5,15 @@ const { validateJWT } = require("../middlewares/validateJWT");
 const api = express.Router();
 
 api.post("/user/change-password", validateJWT, UserController.changePassword);
+api.post(
+  "/user/save-personal-info",
+  validateJWT,
+  UserController.savePersonalInfo
+);
+api.post(
+  "/user/save-financial-info",
+  validateJWT,
+  UserController.saveFinancialInfo
+);
 
 module.exports = api;

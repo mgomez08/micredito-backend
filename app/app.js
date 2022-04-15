ad85ece5e0fb2ee1beef8c90948e457e02ef2a59
Marlon Gómez <marlonjulian15@gmail.com>
2022-04-15 10:14:28 -0500

    Use authRoutes

--- app/app.js
+++ app/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { API_VERSION } = require("../config/variables");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 //Load routes
-
+const authRoutes = require("./routers/auth");
 //Routes
+app.use(`/api/${API_VERSION}`, authRoutes);
 
 module.exports = app;

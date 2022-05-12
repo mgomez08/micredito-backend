50f5cc0c9396afe27b33f75f7f92fc5c17037192
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-12 12:02:05 -0500

    Create bank router

---
+++ app/routers/bank.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const BankController = require("../controllers/bank");
+const validateJWT = require("../middlewares/validateJWT");
+const isAdmin = require("../middlewares/isAdmin");
+
+const api = express.Router();
+
+api.post("/bank/", validateJWT, isAdmin, BankController.createBank);
+api.put("/bank/:id", validateJWT, isAdmin, BankController.editBank);
+api.delete("/bank/:id", validateJWT, isAdmin, BankController.deleteBank);
+api.get("/bank/", validateJWT, isAdmin, BankController.getAllBanks);
+
+module.exports = api;

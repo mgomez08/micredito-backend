578e7a1f39717471e6d04028090466b5dd5c7fea
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-13 11:01:34 -0500

    Create service router

---
+++ app/routers/service.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ServiceController = require("../controllers/service");
+const validateJWT = require("../middlewares/validateJWT");
+const isAdmin = require("../middlewares/isAdmin");
+
+const api = express.Router();
+
+api.post("/service/", validateJWT, isAdmin, ServiceController.createService);
+api.put("/service/:id", validateJWT, isAdmin, ServiceController.editService);
+api.delete(
+  "/service/:id",
+  validateJWT,
+  isAdmin,
+  ServiceController.deleteService
+);
+api.get("/service", validateJWT, isAdmin, ServiceController.getAllServices);
+
+module.exports = api;

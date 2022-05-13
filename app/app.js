7fb222bffc9212dd2005f35077e70319ad7136ab
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-13 12:52:33 -0500

    Add Interest router on app

--- app/app.js
+++ app/app.js
@@ -13,11 +13,13 @@ const userRoutes = require("./routers/user");
 const adminRoutes = require("./routers/admin");
 const bankRoutes = require("./routers/bank");
 const serviceRoutes = require("./routers/service");
+const interestRoutes = require("./routers/interest");
 //Routes
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, adminRoutes);
 app.use(`/api/${API_VERSION}`, bankRoutes);
+app.use(`/api/${API_VERSION}`, interestRoutes);
 app.use(`/api/${API_VERSION}`, serviceRoutes);
 
 module.exports = app;
